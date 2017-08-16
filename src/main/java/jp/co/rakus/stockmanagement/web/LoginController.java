5f1aa177fc63f1f520d755ceee9d0d34eb61fe70
matsumoto <yuka.matsumoto@rakus.co.jp>
2017-08-16 16:54:40 +0900

    (2-1)書籍一覧画面と書籍詳細画面にメンバー名を表示させるように変更

--- src/main/java/jp/co/rakus/stockmanagement/web/LoginController.java
+++ src/main/java/jp/co/rakus/stockmanagement/web/LoginController.java
@@ -1,5 +1,7 @@
 package jp.co.rakus.stockmanagement.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,7 +22,9 @@ import jp.co.rakus.stockmanagement.service.MemberService;
 @Controller
 @RequestMapping("/")
 public class LoginController {
-
+	
+	@Autowired
+	private HttpSession session;
 	@Autowired
 	private MemberService memberService;
 
@@ -63,7 +67,7 @@ public class LoginController {
             result.addError(error);
 			return index();
 		}
-		model.addAttribute("member", member);
+		session.setAttribute("member", member);
 		return "redirect:/book/list";
 	}
 }
